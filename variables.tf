63512365b384245a95bbd3ff47abf238da724473
Dave Arnold <darnold@hashicorp.com>
2018-07-02 16:22:51 -0700

    updating

--- variables.tf
+++ variables.tf
@@ -1,40 +1,20 @@
 variable "cluster_name" {
-	default = "insights-gc"
+  default = "insights-gc"
 }
 
 variable "instance_type" {
-	default = "n1-standard-8"
-}
-
-variable "gc_region" {
-  default = "us-east1"
+  default = "n1-standard-8"
 }
 
 variable "gc_zone" {
-	default = "us-east1-b"
+  default = "us-east1-b"
 }
 
 variable "cluster_size" {
-	default = 4
-}
-
-variable "domain" {
-  default = "alphonso.tv"
-}
-
-variable "google_project" {
-  default = "titanium-flash-726"
-}
-
-variable "google_project_id" {
-  default = "927763580433"
+  default = 4
 }
 
 variable "subnet" {}
-
-variable "dns_managed_zone" {
-  default = "main-domain"
-}
 
 variable "CHEF_CLIENT_NAME" {}
 variable "CHEF_VALIDATION_KEY" {}
@@ -43,6 +23,7 @@ variable "CHEF_SERVER_URL" {}
 variable "chef_env" {
   default = "google-compute"
 }
+
 variable "role" {}
 
 variable "hostname" {}
