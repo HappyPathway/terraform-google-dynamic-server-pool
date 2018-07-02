483f8c00b77bc205b43b5abea4b0c81fd918520f
Dave Arnold <darnold@hashicorp.com>
2018-07-02 16:16:55 -0700

    removing dns

--- main.tf
+++ main.tf
@@ -67,21 +67,6 @@ resource "google_compute_instance" "alpha_instance" {
   }
 }
 
-data "google_dns_managed_zone" "dns_zone" {
-  name = "${var.dns_managed_zone}"
-}
-
-resource "google_dns_record_set" "alpha" {
-  count = "${var.cluster_size}"
-  name  = "${var.hostname}${format("%02d", count.index+1)}.${data.google_dns_managed_zone.dns_zone.dns_name}"
-  type  = "A"
-  ttl   = 300
-
-  managed_zone = "${data.google_dns_managed_zone.dns_zone.name}"
-
-  rrdatas = ["${element(google_compute_instance.alpha_instance.*.network_interface.0.access_config.0.assigned_nat_ip, count.index)}"]
-}
-
 output "hostnames" {
   value = "${google_dns_record_set.alpha.*.name}"
 }
